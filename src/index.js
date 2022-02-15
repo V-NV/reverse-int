bdcfaf6eac7b24fc1a1e632ff324d4652a57fb34
V-NV <96265492+V-NV@users.noreply.github.com>
2022-02-16 01:39:46 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function reverse (n) {
+module.exports = function reverse (n){
+
+let c = n;
+
+if (c < 0){c = c * -1}
+
+let a = String(c);
+
+let i = a.length - 1;
+
+console.log(a,c,i);
+
+d = 0;
+i2 = i;
+
+ for(i1 = 1; i1 <= i; i1++)
+ 
+{
+
+b = c % 10;
+
+c = c / 10 - (b / 10);
+
+d = d + b * 10**i2;
+
+i2 = i2 - 1;
+
+console.log(b,c,d,i2);
+
+
+ }
   
+
+d = d+c;
+
+return(d);
 }
